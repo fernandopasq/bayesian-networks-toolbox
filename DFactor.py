b6ea15aff84507b0283a68b1e5783b62de3784c8
Fernando Pasquini <fernandopasq@gmail.com>
2013-02-22 15:44:58 -0300

    Updating with new approach for DFactor

--- DFactor.py
+++ DFactor.py
@@ -1,21 +1,38 @@
+'''
+File: DFactor.py
+Author: Fernando Pasquini
+Description: Class for factor with discrete parameters
+'''
+
 from pylab import *
+import itertools
 
 class DFactor:
-	"""varsdict is a dict whose keys are variables and values are tuples with the possible assuming values
-	for example: { x:(x1, x2, x3), y:(y1, y2) }
 	"""
-	def __init__(self,varsdict):
-		self.varsdict = varsdict
-		numvalues = 1
-		for variable in varsdict.keys():
-			numvalues = numvalues * len(varsdict[variable])
-		self.values = zeros((numvalues))
-		print self.values
+	Discrete Factor to use in calculations.
+	self.possibilities: all combinations of variables in the factor
+	self.values: values for all the tuples
+	"""
 
-	def getValueIndex(vars_values_dict):
-		indexes = dict(zip(self.varsdict.keys(),[0]*len(self.varsdict.keys())))
-		for variable in vars_values_dict.keys():
-			if(self.varsdict[variable]):
-				indexes[variable] = 
-			else:
-				print "ERROR!!!"
+	def __init__(self,varstuples):
+		""" varstuples: ((x1,x2,x3), (y1, y2))"""
+		self.possibilities = list(itertools.product(*varstuples))
+		self.values = dict.fromkeys(self.possibilities)
+
+	def setValueForVariables(self,varstuple,val):
+		"""sets the value indicated *val* to the combination of variables indicated in the tuple *varstuple* """
+		self.values[varstuple] = val
+
+class DPDF(DFactor):
+	""" Probability Density Fucntion"""
+	def __init__(self, varstuples):
+		super(DPDF, self).__init__()
+		
+	"""pdf related functions"""
+	def initializePdf(self):
+		"""initializes factor as a pdf (sum = 1)"""
+		pass
+
+	def checkPdf(self):
+		"""returns an error if the values in the factor don't correspond to pdfs"""
+		pass
